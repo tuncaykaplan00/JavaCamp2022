e0ba6ebb459ba60079d4b5a5cee21bbb06e71e6c
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 17:51:09 +0300

    first

--- switchDemo/src/switchDemo/Main.java
+++ switchDemo/src/switchDemo/Main.java
@@ -3,7 +3,28 @@ package switchDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char grade = 'C';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		// B ve C case durumları bu şekilde yazılırsa ortak cevabı verir.
+		case 'B':
+		case 'C':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef kaldınız");
+			break;
+		default:
+			System.out.println("Geçersiz not girdiniz");
+			// Default bize aksi durumlardaki cevabı vermektedir.
+			// Ctrl+Shift+F Format Code
+		}
 
 	}
 
