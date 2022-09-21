0b0044d985b572e6a66a4a1e52d5b361c2acc190
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 16:23:59 +0300

    first

--- variables/src/variables/variables.java
+++ variables/src/variables/variables.java
@@ -1,13 +1,22 @@
 package variables;
 
 public class variables {
+	
+	// sayi="Ankara" olamaz çünkü int= tam sayı olmalıdır.
+	// boolean true veya false olarak kullanılır.
+	// boolean dogruMu = false;
+	// char = 'A' Tek tırnak olmalıdır.
+	
 
 	public static void main(String[] args) {
-		int ogrenciSayısı = 12 ;
-		String mesaj = "Öğreci Sayısı : " ;
+		double sayi = 12.5;
+		sayi = 1277;
 		
-		System.out.println(mesaj + ogrenciSayısı);
-		System.out.println("Öğrenci Sayısı : " + ogrenciSayısı);
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		
+		
 	}
 
 	}
