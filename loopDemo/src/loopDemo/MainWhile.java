c6f973f8e41d03b5d40f917b41bcc06422f3e85d
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 19:21:31 +0300

    first

--- loopDemo/src/loopDemo/MainWhile.java
+++ loopDemo/src/loopDemo/MainWhile.java
@@ -10,14 +10,25 @@ public class MainWhile {
 			System.out.println(i);
 		}
 		System.out.println("For Döngüsü Bitti.");
-		int i=2;
+
 		// While
+		int i = 2;
 		while (i < 10) {
 			System.out.println(i);
-			i+=2;
+			i += 2;
 		}
 
 		System.out.println("While Döngüsü Bitti.");
+
+		// Do-While
+		int j = 100;
+		do {
+			System.out.println(j);
+			j += 2;
+
+		} while (j < 10);
+
+		System.out.println("Do-While Döngüsü Bitti.");
 	}
 
 }
