4122765e965e4585f24fcd95bdff0664029c68eb
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 16:52:43 +0300

    first

--- conditionals/src/conditionals/Main.java
+++ conditionals/src/conditionals/Main.java
@@ -3,8 +3,15 @@ package conditionals;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sayi = 19;
+		if (sayi<20) {
+			System.out.println("Sayı 20'den küçüktür.");
+		} else if (sayi==20) {
+			System.out.println("Sayı 20'ye eşittir.");
+		} else {
+			System.out.println("Sayı 20'den büyüktür.");
+		}
+		
 	}
 
 }
