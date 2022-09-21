a308550309297a361ea569d5e2c323f11828ad6e
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 16:25:29 +0300

    first

--- datatypes/src/datatypes/datatypes.java
+++ datatypes/src/datatypes/datatypes.java
@@ -1,10 +1,22 @@
 package datatypes;
 
 public class datatypes {
+	
+	// sayi="Ankara" olamaz çünkü int= tam sayı olmalıdır.
+	// boolean true veya false olarak kullanılır.
+	// boolean dogruMu = false;
+	// char = 'A' Tek tırnak olmalıdır.
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double sayi = 12.5;
+		sayi = 1277;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		
+		
 	}
 
-}
+	}
