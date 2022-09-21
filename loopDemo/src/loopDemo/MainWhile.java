9e9f034194ffa49305b551fedd17dd76f15d1b13
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 19:12:19 +0300

    first

--- loopDemo/src/loopDemo/MainWhile.java
+++ loopDemo/src/loopDemo/MainWhile.java
@@ -3,13 +3,21 @@ package loopDemo;
 public class MainWhile {
 
 	public static void main(String[] args) {
-		//For
+		// For
 		// i++ ile i=i+1 aynı durumdur.
-		//i+=2 komutu i'yi 2 artır demektir.
-		for(int i=2;i<10;i+=2) {
+		// i+=2 komutu i'yi 2 artır demektir.
+		for (int i = 2; i < 10; i += 2) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü Bitti.");
+		System.out.println("For Döngüsü Bitti.");
+		int i=2;
+		// While
+		while (i < 10) {
+			System.out.println(i);
+			i+=2;
+		}
+
+		System.out.println("While Döngüsü Bitti.");
 	}
 
 }
