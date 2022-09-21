83cfa15559d919d61a7c542e596647275020c9b7
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-21 16:04:08 +0300

    first

---
+++ variables/src/variables/variables.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayısı = 12 ;
+		String mesaj = "Öğreci Sayısı : " ;
+		
+		System.out.println(mesaj + ogrenciSayısı);
+		System.out.println("Öğrenci Sayısı : " + ogrenciSayısı);
+	}
+
+	}
