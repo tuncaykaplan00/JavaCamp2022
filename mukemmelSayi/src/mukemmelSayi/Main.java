6f9e8e13ec88c9b2b13f17b1bea5b1ff99b08d7f
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-27 20:52:01 +0300

    First

---
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,27 @@
+package mukemmelSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 6 ---> 1,2,3
+		// 28 --> 1,2,4,7,14
+
+		int number = 28;
+		int total = 0;
+
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				total = total + i;
+			}
+
+		}
+
+		if (total == number) {
+			System.out.println("Sayı mükemmel sayıdır.");
+		} else {
+			System.out.println("Sayı mükemmel sayı değildir.");
+		}
+
+	}
+
+}
